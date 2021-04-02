851957c51bec09bdbec75cdb7abfcb2755a81b56
wbwd <77216609+kmcpherson49@users.noreply.github.com>
2021-04-02 15:53:28 -0700

    initial setup

---
+++ Develop/index.js
@@ -0,0 +1,27 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs"); 
+const generateMarkdown = require("./utils/generateMarkdown")
+// TODO: Create an array of questions for user input
+const questions = [{
+    type: "input",
+    name: "name",
+    message: 'What is your name?'
+}];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+    .prompt(
+        questions
+    )
+    .then(answers => {
+        fs.writeFileSync("README.md", generateMarkdown(answers));
+    })
+}
+
+// Function call to initialize app
+init();
